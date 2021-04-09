c9200190ab8887ad34aa27ad8b00b00bd622a489
jianghaoting <jianghaoting@cnstrong.cn>
2021-04-09 22:06:41 +0800

    mq

---
+++ producer/src/main/java/com/example/demo/controller/SendMessageController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * @author JiangHaoTing
+ * @create: 2021-04-09 19:44
+ */
+
+
+@RequestMapping("rabbitmq")
+@RestController
+public class SendMessageController {
+
+    @Resource
+    RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/sendDirectMessage")
+    public String sendDirectMessage() {
+        String messageId = String.valueOf(UUID.randomUUID());
+        String messageData = "test message, Hello Word!";
+        String createTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        Map<String, Object> map = new HashMap<>();
+        map.put("messageId", messageId);
+        map.put("messageData", messageData);
+        map.put("createTime", createTime);
+        //将消息携带绑定键值：TestDirectRouting 发送到交换机TestDirectExchange
+        rabbitTemplate.convertAndSend("testMyQueue", "myTest", map);
+        return "ok";
+    }
+
+}
---
+++ producer/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+
+
+    @Test
+    void contextLoads() throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            String messageId = String.valueOf(UUID.randomUUID());
+            String messageData = "test message, Hello Word!";
+            String createTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            Map<String, Object> map = new HashMap<>();
+            map.put("messageId", messageId);
+            map.put("messageData", messageData);
+            map.put("createTime", createTime);
+            //将消息携带绑定键值：TestDirectRouting 发送到交换机TestDirectExchange
+            rabbitTemplate.convertAndSend("testMyQueue", "myTest", map);
+//            Thread.sleep(10000);
+        }
+
+    }
+
+}
